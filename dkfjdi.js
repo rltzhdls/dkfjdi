4d80ec512af9ab705d83752fc9be4cc79b79c66b
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-21 22:02:23 +0900

    Add files via upload

---
+++ dkfjdi.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/cd@main/bloodpledge.webm";
+        document.getElementById('vd').load();
+    }
+});
+
